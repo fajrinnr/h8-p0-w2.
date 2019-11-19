7f0a435af8389268cd42c7ba8a86400ed9c63e69
fajrinnr <fajrin.noorrachman11@gmail.comgit config --global user.name fajrinnrgitgit config --global user.email fajrin.noorrachman11@gmail.com>
2019-11-19 14:26:23 +0700

    Menggunakan Switch-Case

---
+++ exercise-4.js
@@ -0,0 +1,45 @@
+var hari = 11
+var bulan = 10
+var tahun = 1997
+var bulanString
+
+switch (bulan) {
+    case 1 :
+    bulanString = 'Januari'
+    break
+    case 2 :
+    bulanString = 'Februari'
+    break
+    case 3 :
+    bulanString = 'Maret'
+    break
+    case 4 :
+    bulanString = 'April'
+    break
+    case 5 :
+    bulanString = 'Mei'
+    break
+    case 6 :
+    bulanString = 'Juni'
+    break
+    case 7 :
+    bulanString = 'Juli'
+    break
+    case 8 :
+    bulanString = 'Agustus'
+    break
+    case 9 :
+    bulanString = 'September'
+    break
+    case 10 :
+    bulanString = 'Oktober'
+    break
+    case 11 :
+    bulanString = 'November'
+    break
+    case 12 :
+    bulanString = 'Desember'
+    break
+}
+    
+console.log(hari + ' ' + bulanString + ' ' + tahun)
