33cf7b51ebacb26d7656703bcf67b984f6573e8f
fajrinnr <fajrin.noorrachman11@gmail.comgit config --global user.name fajrinnrgitgit config --global user.email fajrin.noorrachman11@gmail.com>
2019-11-20 10:49:04 +0700

    Logic Challenge - Konversi Menit

--- exercise-12.js
+++ exercise-12.js
@@ -2,14 +2,14 @@
 function konversiMenit(menit) {
     var menitKe = Math.floor(menit / 60);
     var detikKe = (menit %60)
-    if (detikKe <= 10)
+    if (detikKe < 10)
     return menitKe + ':0' +  detikKe
     else
     return menitKe + ':' + detikKe
   }
   
   // TEST CASES
-  console.log(konversiMenit(63)); // 1:03
+  console.log(konversiMenit(70)); // 1:03
   console.log(konversiMenit(124)); // 2:04
   console.log(konversiMenit(53)); // 0:53
   console.log(konversiMenit(88)); // 1:28
