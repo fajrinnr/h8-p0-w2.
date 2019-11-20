0d58357232e6a00bf515286e0aa66d7792af4bba
fajrinnr <fajrin.noorrachman11@gmail.comgit config --global user.name fajrinnrgitgit config --global user.email fajrin.noorrachman11@gmail.com>
2019-11-20 19:55:00 +0700

    Logic Challenge - X dan O

---
+++ exercise-13.js
@@ -0,0 +1,23 @@
+// Logic Challenge - X dan O //
+function xo(str) {
+    var x = 0
+    var o = 0
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === 'x')
+        x++
+        else
+        o++
+    }
+    if (x === o)
+    return true
+    else
+    return false
+}
+
+
+  // TEST CASES
+  console.log(xo('xoxoxo')); // true
+  console.log(xo('oxooxo')); // false
+  console.log(xo('oxo')); // false
+  console.log(xo('xxxooo')); // true
+  console.log(xo('xoxooxxo')); // true
