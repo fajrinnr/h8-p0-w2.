b4a295b0a701ac7c34201c3cfd5c8e509bf623e5
fajrinnr <fajrin.noorrachman11@gmail.comgit config --global user.name fajrinnrgitgit config --global user.email fajrin.noorrachman11@gmail.com>
2019-11-22 22:57:55 +0700

    Logic Challenge - X dan O

--- exercise-13.js
+++ exercise-13.js
@@ -3,17 +3,20 @@ function xo(str) {
     var x = 0
     var o = 0
     for (let i = 0; i < str.length; i++) {
-        if (str[i] === 'x')
-        x++
-        else
-        o++
+        if (str[i] === 'x') {
+            x++
+        } else {
+            o++
+        }
+       
     }
-    if (x === o)
+    if (x === o) {
     return true
-    else
+    }
+    else {
     return false
+    }
 }
-
 
   // TEST CASES
   console.log(xo('xoxoxo')); // true
--- exercise-3.js
+++ exercise-3.js
@@ -3,37 +3,38 @@ var nama = 'Jampang'
 var peran = 'Tabib'
 
 // Conditional Kosong //
-if (nama === '' && peran === '')
+if (nama === '' && peran === '') {
 console.log("Nama harus diisi!")
-
+}
 // Conditional peran kosong //
-else if (nama === 'Fajrin' && peran === '')
+else if (nama === 'Fajrin' && peran === '') {
 console.log("Halo Fajrin, Pilih peranmu untuk memulai game!")
-else if (nama === 'Ihsan' && peran === '')
+} else if (nama === 'Ihsan' && peran === '') {
 console.log("Halo Ihsan, Pilih peranmu untuk memulai game!")
-else if (nama === 'Jampang' && peran === '')
+} else if (nama === 'Jampang' && peran === '') {
 console.log("Halo Jampang, Pilih peranmu untuk memulai game!")
-
+}
 // Conditional Fajrin //
-else if (nama === 'Fajrin' && peran === 'Ksatria')
+else if (nama === 'Fajrin' && peran === 'Ksatria') {
 console.log(`Halo ${peran} ${nama}, kamu dapat menyerang dengan senjatamu!`)
-else if (nama === 'Fajrin' && peran === 'Tabib')
+} else if (nama === 'Fajrin' && peran === 'Tabib') {
 console.log(`Halo ${peran} ${nama}, kamu akan membantu temanmu yang terluka.`)
-else if (nama === 'Fajrin' && peran === 'Penyihir')
+} else if (nama === 'Fajrin' && peran === 'Penyihir') {
 console.log(`Halo ${peran} ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`)
-
+}
 // Conditional Ihsan //
-else if (nama === 'Ihsan' && peran === 'Ksatria')
+else if (nama === 'Ihsan' && peran === 'Ksatria') {
 console.log(`Halo ${peran} ${nama}, kamu dapat menyerang dengan senjatamu!`)
-else if (nama === 'Ihsan' && peran === 'Tabib')
+} else if (nama === 'Ihsan' && peran === 'Tabib') {
 console.log(`Halo ${peran} ${nama}, kamu akan membantu temanmu yang terluka.`)
-else if (nama === 'Ihsan' && peran === 'Penyihir')
+} else if (nama === 'Ihsan' && peran === 'Penyihir') {
 console.log(`Halo ${peran} ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`)
-
+}
 // Conditional Jampang //
-else if (nama === 'Jampang' && peran === 'Ksatria')
+else if (nama === 'Jampang' && peran === 'Ksatria') {
 console.log(`Halo ${peran} ${nama}, kamu dapat menyerang dengan senjatamu!`)
-else if (nama === 'Jampang' && peran === 'Tabib')
+} else if (nama === 'Jampang' && peran === 'Tabib') {
 console.log(`Halo ${peran} ${nama}, kamu akan membantu temanmu yang terluka.`)
-else if (nama === 'Jampang' && peran === 'Penyihir')
+} else if (nama === 'Jampang' && peran === 'Penyihir') {
 console.log(`Halo ${peran} ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`)
+}
